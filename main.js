3283989a8f24d32f7ae70fd188f8658b7676d29e
Symfomany <zuzu38080@gmail.com>
2018-03-15 17:12:02 +0100

    add a title for Sylvain

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class componentName extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h1>Hello!</h1>
+      </div>
+    );
   }
 }
